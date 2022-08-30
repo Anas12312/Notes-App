a448938e061510812ce71c8cf09d3001be2aaa4d
unknown <anashesham000@gmail.com>
2022-08-30 18:34:32 +0200

    Every Thing

---
+++ edit-notes.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const title = document.querySelector('#edit-title')
+const body = document.querySelector('#edit-body')
+const removeBtn = document.querySelector('#remove-note')
+const lastUpdate = document.querySelector('#last-update')
+
+const noteId = location.hash.substring(1)
+let notes = getSavedNotes()
+let note = notes.find((item) => item.id === noteId)
+if(!note){
+    location.assign('/index.html')
+}
+lastUpdate.textContent = `Last edit was ${moment(note.updatedAt).fromNow()}`
+title.value = note.title
+body.value = note.body
+
+title.addEventListener('input',(e) => {
+    note.title = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastUpdate.textContent = `Last edit was ${moment(note.updatedAt).fromNow()}`
+    saveNotes(notes)
+})
+
+
+body.addEventListener('input',(e) => {
+    note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastUpdate.textContent = `Last edit was ${moment(note.updatedAt).fromNow()}`
+    saveNotes(notes)
+})
+
+removeBtn.addEventListener('click',() => {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        const note = notes.find((item) => item.id === noteId)
+        if(!note){
+            location.assign('/index.html')
+        }
+        title.value = note.title
+        body.value = note.body
+        lastUpdate.textContent = `Last edit was ${moment(note.updatedAt).fromNow()}`
+    }
+})
---
+++ notes-app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let notes = getSavedNotes()
+const filters = {
+    searchText: '',
+    sortBy: 'byEdited'
+}
+renderNotes(notes, filters)
+document.querySelector('#create-note').addEventListener('click',(e) => {
+    const timestamp = moment().valueOf()
+    const newNote = {
+        id: uuidv4(),
+        title: '',
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
+    }
+    notes.push(newNote)
+    saveNotes(notes)
+    renderNotes(notes, filters)
+    location.assign(`/edit.html#${newNote.id}`)
+})
+
+document.querySelector('#search-text').addEventListener('input',(e) => {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change',(e) => {
+    filters.sortBy = e.target.value
+    renderNotes(notes,filters)
+})
+
+window.addEventListener('storage', (e) => {
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes,filters)
+    }
+})
